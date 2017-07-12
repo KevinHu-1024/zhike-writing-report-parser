8423588a0625c89f0bacc8227246616984e27e69
kevin <huke@innobuddy.com>
2017-07-12 14:27:11 +0800

    fix: 异常处理

--- src/loaders/markInSentence.ts
+++ src/loaders/markInSentence.ts
@@ -1,20 +1,27 @@
 import SlotChildren from '../interfaces/SlotChildren'
 export default function markInSentence(...marks: SlotChildren[]): SlotChildren[] {
-  const sentences = this.reportJSON.reportBySentence;
+  const sentences = this.reportJSON.reportBySentence || [];
   this._data.sentences = sentences
   
-  for(let mark of marks) {
-    mark.sentIndex = 0;
+  if (sentences.length !== 0) {
+    try {
+      for(let mark of marks) {
+        mark.sentIndex = 0;
 
-    for (let i = 0; i < sentences.length; i++) {
-      const current = sentences[i].beginPosition
-      const next = sentences[i + 1] && sentences[i + 1].beginPosition
-      if (mark.start >= current) {
-        if (mark.end <= next || !next) {
-          mark.sentIndex = i
+        for (let i = 0; i < sentences.length; i++) {
+          const current = sentences[i].beginPosition
+          const next = sentences[i + 1] && sentences[i + 1].beginPosition
+          if (mark.start >= current) {
+            if (mark.end <= next || !next) {
+              mark.sentIndex = i
+            }
+          }
         }
-      }
+      } 
+    } catch (error) {
+      console.log(error)
     }
   }
+  
   return marks;
 }
